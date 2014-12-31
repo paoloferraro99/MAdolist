6e41f047e908b7cdf52a7b4dd2986215e47ae2b3
paoloferraro99 <paoloferraro99@gmail.com>
2014-12-31 20:01:20 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MAdolist::Application.config.secret_key_base = 'bf972615616a3ffc450dc58f14a121314575cc337f911e3a7da33db0803e351be15d79358af8e9bc8b840207970db1f6794b70fd6d6ee3522230e982e1990dc4'
